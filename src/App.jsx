791e66390469e5723a0109459d1e63e537d429f7
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-12-12 22:37:55 -0300

    card template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 //import { useState } from 'react'
 import './App.css'
+import CardPokemon from './components/CardPokemon/CardPokemon'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
     return (
         <>
             <Header />
+            <CardPokemon/>
             <Footer />
         </>
     )
