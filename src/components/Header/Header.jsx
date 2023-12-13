ae6e73ae810c5a39387c9a5db0932a9bcfd25540
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-12-12 21:42:38 -0300

    header finalizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import { Icon } from "@iconify/react"
 import './Header.css'
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const Header = () => {
     const [show, setShow] = useState(null);
+    const inputRef = useRef(null)
 
     const handleInputShow = () => {
-        show ? setShow(null) : setShow(true)
+        show ? setShow(null) : setShow(true);
+        if (inputRef){
+            inputRef.current.setAttribute("autofocus", true)
+        }
     }
     const handleInputBlur = () =>{
         setShow(null)
@@ -15,7 +19,7 @@ const Header = () => {
     return (
         <header>
             <div className="brand">
-                <img src="" alt="logo" />
+                <img src="assets/pngegg (11).png" alt="logo" />
                 <h1>GabiDex</h1>
             </div>
             <nav className="nav">
@@ -24,9 +28,12 @@ const Header = () => {
                     onClick={handleInputShow}
                     />
                 <input 
-                    type="search" 
+                    id="busqueda"
+                    type="text" 
                     className={show ? "" : "none"}
-                    onBlur={handleInputBlur}/>
+                    onBlur={handleInputBlur}
+                    ref={inputRef}
+                    />
             </nav>
         </header>
     )
